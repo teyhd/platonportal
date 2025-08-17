1a14a88b702486843968194e8969f51458567ce3
vladteyhd <vladteyhd@mail.ru>
2025-08-17 04:53:26 +0300

    cardfix

--- vendor/db.mjs
+++ vendor/db.mjs
@@ -3,7 +3,7 @@ let sets = {
     host: process.env.MDBHOST,
     user: process.env.DBUSER,
     password : process.env.DBPASS,
-    database: process.env.DBNAMES,
+    database: process.env.DBNAMESUSR,
     charset : 'utf8mb4_general_ci',
     waitForConnections: true,
     connectionLimit: 100,
@@ -13,16 +13,24 @@ let sets = {
     enableKeepAlive: false,
     keepAliveInitialDelay: 0
 }
-const pool = mysql.createPool(sets).promise()
+const usr = mysql.createPool(sets).promise()
+sets.database = process.env.DBNAMES
+const portal = mysql.createPool(sets).promise()
 
 export async function auth_user(pin){
     const qer = `SELECT id,name,type as role FROM users WHERE pin = ${pin}`
-    const [rows, fields] = await pool.query(qer)
+    const [rows, fields] = await usr.query(qer)
     return rows[0];
 }
 
 export async function get_roles(uid){
   const qer = `SELECT max(ROLE) as role FROM roles WHERE user_id = ${uid}`
-  const [rows, fields] = await pool.query(qer)
+  const [rows, fields] = await usr.query(qer)
+  return rows;
+}
+
+export async function get_cards(role=0){
+  const qer = `SELECT * FROM cards WHERE role <= ${role} AND shows = 1 order by crdorder`
+  const [rows, fields] = await portal.query(qer)
   return rows;
 }
